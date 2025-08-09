c17ce450c86b078123ec372d16e155a6146e2d08
Tanveer Ahmad <tanveerahmad60@gmail.com>
2025-08-10 01:13:02 +0530

    Initial commit

---
+++ python Asg 1.py
@@ -0,0 +1,23 @@
+#Task 1 Problem Statement: Write a Python program that does the take two input and :
+a = input("Enter No 1:")
+b = input("Enter No 2 : ")
+a=int(a)
+b=int(b)
+print('Addition:', a+b )
+print( 'Substraction:', a-b)
+print('multiplication:', a*b)
+print('division:',a/b)
+
+
+#_________________________________________________________________
+
+#Task 2 Problem Statement: Write a Python program that does the following:
+# Write a Python program that:
+# Takes a user's first name and last name as input. Concatenates the first name and last name into a full name.
+# Prints a personalized greeting message using the full name.
+
+c=input("Enter your first Name: ")
+d=input("Enter your Last Name: ")
+c=str(c)
+d=str(d)
+print("Hello,"+ c + " " + d +  " " + "Welcome to the python Program.")
