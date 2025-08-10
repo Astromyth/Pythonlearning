4d30491465ecc00a9af009baeaebebc6fc7d3d03
Tanveer Ahmad <tanveerahmad60@gmail.com>
2025-08-11 00:11:46 +0530

    assignment 2

---
+++ range.py
@@ -0,0 +1,24 @@
+#print  range
+#n=list(range(2,11,1))
+#print(n)
+
+#----------------------
+
+#print loops
+
+#list=['tanveer',10.2,1980]
+#print(list)
+#for i in list:
+    #print (i)
+#___________________________________
+#for loop2
+
+#for i in range(0,12):
+    #print ('a')
+
+    #____________________________
+ # while loop  print a 5 times
+'''
+while (counter <10):
+    print("python")
+    counter =counter + 1
